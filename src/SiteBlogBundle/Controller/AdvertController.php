9e31129e5643e4fb60e8fe704636c394e107ede0
Maxime <richard.maxime9@gmail.com>
2016-10-22 21:56:48 +0200

    Add view advert

--- src/SiteBlogBundle/Controller/AdvertController.php
+++ src/SiteBlogBundle/Controller/AdvertController.php
@@ -3,7 +3,10 @@
 namespace SiteBlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdvertController extends Controller
 {
@@ -18,19 +21,9 @@ class AdvertController extends Controller
 
     public function viewAction($id)
     {
-        return new Response("Affichage de l'annonce d'id : ".$id);
-    }
-
-    public function addAction()
-    {
-        
-    }
-
-    public function viewSlugAction($slug, $year, $format)
-    {
-        return new Response(
-            "On pourrait afficher l'annonce correspondant au
-            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        return $this->render(
+            'SiteBlogBundle:Advert:view.html.twig',
+            array('id'  => $id)
         );
     }
 
