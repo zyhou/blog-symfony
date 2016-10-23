9f14736d031fbfc29ad1cee2cc6d441dfbd543d5
Maxime <richard.maxime9@gmail.com>
2016-10-23 22:01:37 +0200

    Ajout view crud avec layout

--- src/SiteBlogBundle/Controller/AdvertController.php
+++ src/SiteBlogBundle/Controller/AdvertController.php
@@ -13,18 +13,48 @@ class AdvertController extends Controller
     public function indexAction($page)
     {
         if($page < 1) {
-            throw new NotFoundHttpException('Page"'.$age.'" inexistante');
+            throw new NotFoundHttpException('Page "'.$page.'" inexistante');
         }
 
-        return $this->render('SiteBlogBundle:Advert:index.html.twig');
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony2',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+
+        return $this->render('SiteBlogBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
     }
 
     public function viewAction($id)
     {
-        return $this->render(
-            'SiteBlogBundle:Advert:view.html.twig',
-            array('id' => $id)
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
         );
+
+        return $this->render('SiteBlogBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));
     }
 
     public function addAction(Request $request)
@@ -37,9 +67,41 @@ class AdvertController extends Controller
         return $this->render('SiteBlogBundle:Advert:add.html.twig');
     }
 
+    public function editAction($id, Request $request)
+    {
+        if($id < 1) {
+            throw new NotFoundHttpException('Annonce "'.$id.'" inexistante');
+        }
+
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('SiteBlogBundle:Advert:edit.html.twig', array(
+            'advert' => $advert
+        ));
+    }
+
     public function deleteAction($id)
     {
         return $this->render('SiteBlogBundle:Advert:delete.html.twig');
+    }
+
+    public function menuAction()
+    {
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('SiteBlogBundle:Advert:menu.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
     }
 
 }
