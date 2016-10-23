30c7e4140c3b0ef84762da065b80d9d20fbe17f3
Maxime <richard.maxime9@gmail.com>
2016-10-23 19:35:06 +0200

    Mise en place action add/delete

--- src/SiteBlogBundle/Controller/AdvertController.php
+++ src/SiteBlogBundle/Controller/AdvertController.php
@@ -10,21 +10,36 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
-        $content = $this->get('templating')->render('SiteBlogBundle:Advert:index.html.twig', array(
-            'nom' => 'Maxime',
-            'advert_id' => '5'
-        ));
-        return new Response($content);
+        if($page < 1) {
+            throw new NotFoundHttpException('Page"'.$age.'" inexistante');
+        }
+
+        return $this->render('SiteBlogBundle:Advert:index.html.twig');
     }
 
     public function viewAction($id)
     {
         return $this->render(
             'SiteBlogBundle:Advert:view.html.twig',
-            array('id'  => $id)
+            array('id' => $id)
         );
+    }
+
+    public function addAction(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+            return $this->redirectToRoute('site_blog_view', array('id' => 5));
+        }
+
+        return $this->render('SiteBlogBundle:Advert:add.html.twig');
+    }
+
+    public function deleteAction($id)
+    {
+        return $this->render('SiteBlogBundle:Advert:delete.html.twig');
     }
 
 }
