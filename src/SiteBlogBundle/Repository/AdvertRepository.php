f81b6401e4ba840f5e1066c3ffc11f0443414f15
Maxime <richard.maxime9@gmail.com>
2016-10-26 18:34:55 +0200

    Ajout méthode repository pour advert et application

--- src/SiteBlogBundle/Repository/AdvertRepository.php
+++ src/SiteBlogBundle/Repository/AdvertRepository.php
@@ -10,4 +10,15 @@ namespace SiteBlogBundle\Repository;
  */
 class AdvertRepository extends \Doctrine\ORM\EntityRepository
 {
+
+    public function getAdvertWithCategories(array $categoryNames)
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb->join('a.categories', 'c')->addSelect('c');
+        $qb->where($qb->expr()->in('c.name', $categoryNames));
+
+        return $qb->getQuery()->getResult();
+    }
+
+
 }
--- src/SiteBlogBundle/Repository/ApplicationRepository.php
+++ src/SiteBlogBundle/Repository/ApplicationRepository.php
@@ -10,4 +10,14 @@ namespace SiteBlogBundle\Repository;
  */
 class ApplicationRepository extends \Doctrine\ORM\EntityRepository
 {
+
+    public function getApplicationsWithAdvert($limit)
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb->join('a.advert', 'adv')->addSelect('adv');
+        $qb->setMaxResults($limit);
+
+        return $qb->getQuery()->getResult();
+    }
+
 }
